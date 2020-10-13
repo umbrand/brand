45477a7dce35e0a53856280aac5ede7cbc140078
yahiaali <yahiaali124@gmail.com>
2020-10-13 11:45:20 -0400

    Add redis_test script

---
+++ proc/mouse_read/redis_test.cpp
@@ -0,0 +1,90 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h> /* close() */
+#include <pthread.h>
+#include <fcntl.h> // File control definitions
+#include <linux/input.h>
+#include <signal.h>
+#include "redisTools.h"
+#include "hiredis.h"
+
+// List of parameters read from the yaml file, facilitates function definition of initialize_parameters
+typedef struct yaml_parameters_t {
+    int num_channels;
+    int samples_per_redis_stream;
+} yaml_parameters_t;
+
+
+void initialize_redis();
+void initialize_signals();
+void handler_SIGINT(int exitStatus);
+void initialize_parameters(yaml_parameters_t *p);
+
+char PROCESS[] = "redis_test";
+redisReply *reply;
+redisContext *redis_context;
+
+int flag_SIGINT = 0;
+
+int main() {
+    initialize_redis();
+    initialize_signals();
+
+    yaml_parameters_t yaml_parameters = {0};
+    initialize_parameters(&yaml_parameters);
+}
+
+
+void initialize_redis() {
+
+    printf("[%s] Initializing Redis...\n", PROCESS);
+
+    char redis_ip[]       = "127.0.0.1";
+    char redis_port[]     = "6379";
+
+    // load_YAML_variable_string(PROCESS, "redis_ip",   redis_ip,   sizeof(redis_ip));
+    // load_YAML_variable_string(PROCESS, "redis_port", redis_port, sizeof(redis_port));
+
+    printf("[%s] From YAML, I have redis ip: %s, port: %s\n", PROCESS, redis_ip, redis_port);
+
+    printf("[%s] Trying to connect to redis.\n", PROCESS);
+
+    redis_context = redisConnect(redis_ip, atoi(redis_port));
+    if (redis_context->err) {
+        printf("[%s] Redis connection error: %s\n", PROCESS, redis_context->errstr);
+        exit(1);
+    }
+
+    printf("[%s] Redis initialized.\n", PROCESS);
+}
+
+void initialize_signals() {
+
+    printf("[%s] Attempting to initialize signal handlers.\n", PROCESS);
+
+    signal(SIGINT, &handler_SIGINT);
+
+    printf("[%s] Signal handlers installed.\n", PROCESS);
+}
+
+//------------------------------------
+// Handler functions
+//------------------------------------
+
+void handler_SIGINT(int exitStatus) {
+    flag_SIGINT++;
+}
+
+
+void initialize_parameters(yaml_parameters_t *p) {
+
+    char num_channels_string[16] = {0};
+    char samples_per_redis_stream_string[16] = {0};
+
+    load_YAML_variable_string(PROCESS, "num_channels", num_channels_string,   sizeof(num_channels_string));
+    load_YAML_variable_string(PROCESS, "samples_per_redis_stream", samples_per_redis_stream_string,   sizeof(samples_per_redis_stream_string));
+
+    p->num_channels             = atoi(num_channels_string);
+    p->samples_per_redis_stream = atoi(samples_per_redis_stream_string);
+
+}
