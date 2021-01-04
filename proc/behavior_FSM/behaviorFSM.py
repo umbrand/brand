da506e51fe0e5cca21fd724c11f62cadf9a3b9ec
kb0dkin <kb0dkin@github.com>
2021-01-04 11:49:34 -0600

    updated state information and cursor/target arrays sent to Redis

--- proc/behavior_FSM/behaviorFSM.py
+++ proc/behavior_FSM/behaviorFSM.py
@@ -47,27 +47,35 @@ class target():
     # off, on, or if the cursor is over the target
     
     def __init__(self,tgtDict): # initialize the class
-        self.location = {}
-        self.location['state'] = 'off' # always start with everything off
-        self.location['x'] = tgtDict['x']
-        self.location['y'] = tgtDict['y']
-        self.location['width'] = tgtDict['width']
-        self.location['height'] = tgtDict['height']
+        self.state = 0 # always start with everything off
+        self.x = tgtDict['x']
+        self.y = tgtDict['y']
+        self.width = tgtDict['width']
+        self.height = tgtDict['height']
         
     def off(self): # to turn the state 'off'
-        self.state = 'off'
+        self.state = 0
         
     def on(self): # to turn the state to 'on'
-        self.state = 'on'
+        self.state = 1
     
     def over(self):
-        self.state = 'over' # to turn the state to 'over'
+        self.state = 2 # to turn the state to 'over'
        
-    def isOver(self, cursRect):
-        self.color = 'on'
-        # insert some sort of collision detection here
-        return False
-
+    def isOver(self, curs):
+        self.on
+        x = self.x - curs.x # center everything on the target
+        y = self.y - curs.y
+        # if the cursor is within the target's range
+        if (abs(x) <= self.width/2) and (abs(y) <= self.height/2):
+            self.over
+            return True
+        else:
+            self.on
+            return False
+    
+    def packTarget(self):
+        return pack('Ihhhh',self.state,self.x,self.y,self.width,self.height)
 
 
 # define the cursor
@@ -82,32 +90,67 @@ class cursor:
         self.bX1 = cursDict('gain_x_1')
         self.bY0 = cursDict('gain_y_0')
         self.bY1 = cursDict('gain_y_1')
-        # width and height are just for the collision bounding box
-        self.'width' = cursDict['width'] 
-        self.'height' = cursDict['height']
-        # putting location and on/off info into a dictionary for easy
-        # transfer into Redis. Using byte-strings for easier usage in C
-        self.location = {}
-        self.location[b'state'] = 'off' # always start with everything off
-        self.location[b'x'] = 0 # just initialization
-        self.location[b'y'] = 0
+        # width and height are just for the collision bounding box -- ignore for now
+        self.width = cursDict['width'] 
+        self.height = cursDict['height']
+        # we'll pack the three output values (state, x and y)
+        # into a single byte string later to send to redis
+        self.state = 0 # always start with everything off
+        self.x = 0 # just initialization
+        self.y = 0
 
     def off(self):
-        self.state = 'off'
+        self.state = 0
         
     def on(self):
-        self.state = 'on'
+        self.state = 1
     
-    def update_cursor(self, redisStream, sensor0, sensor1):
-        redisFrame = redisStream.xread({'nidaq_stream','$'}, block=0, count=1)[0][1][0][1]
-        
-        if len(redisFrame) != 0:
-            self.location['x'] = (redisFrame[sensor0]*self.mX0) + (redisFrame[sensor1]*self.mX1) + self.bX
-            self.location['y'] = (redisFrame[sensor0]*self.mY0) + (redisFrame[sensor1]*self.mY1) + self.bY
+    def update_cursor(self, nidaqStream, sensor0, sensor1):
+        s0,s1 = unpack('h',nidaqStream[sensor0]),unpack('h',nidaqStream[sensor1])
+        self.x = (s0*self.mX0) + (s1*self.mX1) + self.bX
+        self.y = (s0*self.mY0) + (s1*self.mY1) + self.bY
     
-    def recenter(self):
+    def recenter(self, nidaqStream, sensor0, sensor1):
+        self.update_cursor(nidaqStream,sensor0,sensor1)
         self.bX = -self.x
         self.bY = -self.y
+        self.update_cursor(nidaqStream, sensor0, sensor1)
+    
+    def packCurs(self):
+        return pack('Ihh',self.state,self.x,self.y)
+
+
+# define the touchpad
+class touchpad():
+    def __init__(self,minTouch,maxTouch):
+        self.active = False
+        self.tapped = False
+        self.minTouch,self.maxTouch = minTouch,maxTouch
+        self.touchStart = 0
+    
+    def activate(self,nidaqControl,redisPipe):
+        self.active = True
+        self.touchLength = (self.maxTouch-self.minTouch)*np.random.random() + self.minTouch
+        self.touchStart = 0
+        nidaqControl[b'touch_active'] = pack('?',1)
+        redisPipe.add(b'nidaqControl',nidaqControl)
+        
+    
+    def deactivate(self,nidaqControl,redisPipe):
+        self.active = False
+        nidaqControl[b'touch_active'] = pack('?',0)
+        redisPipe.add(b'nidaqControl',nidaqControl)
+    
+    def tap_check(self,nidaqStream):
+        if unpack('?',nidaqStream[b'touchpad_touched']):
+            if self.touchStart == 0:
+                self.touchStart = dt.now().timestamp()
+                return False
+            elif (dt.now().timestamp() - self.touchStart) > self.touchLength:
+                return True
+        else:
+            self.touchStart = 0
+            return False
 
 
 # storing timing info for between tasks, hold times etc
@@ -161,7 +204,8 @@ STATE_REWARD = 2
 STATE_BETWEEN_TRIALS = 3
 
 state = STATE_BETWEEN_TRIALS
-
+stateTime = 0
+nidaqControl = {b'touch_active':pack('?',0), b'reward':pack('?',0)}
 '''##########################################################
 ### main loop
 ##########################################################'''
@@ -169,22 +213,56 @@ state = STATE_BETWEEN_TRIALS
 while True:
     
     # update the current location of the cursor
-    curs.update_cursor(r, sensor0, sensor1) # sensor names
+    nidaqFrame = redisStream.xread({b'nidaq_stream','$'}, block=0, count=1)[0][1][0][1]
+    curs.update_cursor(nidaqFrame, sensor0, sensor1) # sensor names
+    currTime = dt.now().timestamp() # the posix time at the beginning of the loop
+    p = r.pipeline()
+    p.xadd(b'cursorLocation',curs.packCurs)
+    p.xadd(b'targetLocation',tgt.packTarget)
     
     if state == STATE_START_TRIAL:
-        
+        if tpad.tap_check(nidaqFrame):
+            p.xadd(b'state',{b'state': b'movement', b'time': currTime})
+            state = STATE_MOVEMENT
+            stateTime = 0
+            tPad.deactivate(nidaqControl,p)
     
-    if state = STATE_MOVEMENT:
+    if state == STATE_MOVEMENT:
+        if tgt.isOver(curs):
+            if stateTime == 0:
+                stateTime = currTime
+            elif (dt.now().timestamp() - inTgtTime) > :targetHoldTime
+                state = STATE_REWARD # next state
+                p.xadd(b'state',{b'state':b'movement', b'time': currTime})
+                tgt.off()
+                curs.off()
+                stateTime = currTime
         
+        else: 
+            stateTime = 0 # reset the time over the target
+            
+            
     
-    if state = STATE_REWARD:
+    if state == STATE_REWARD:
+        nidaqControl[b'reward'] == pack('?',True)
+        if (currTime-stateTime) > dispenseTime:
+            state = STATE_BETWEEN_TRIALS
+            p.xadd(b'state',{b'state':b'movement',b'time',currTime})
+            nidaqControl[b'reward'] = pack('?',False)
+            stateTime = currTime
         
+        p.add(b'nidaqControl',nidaqControl)
     
-    if state = STATE_BETWEEEN_TRIALS:
-        
+    if state == STATE_BETWEEEN_TRIALS:
+        if (currTime-stateTime) > interTrialTime:
+            tgt = restart_task(targets)
+            tPad.activate(nidaqControl,p)
+            targetHoldTime.reroll()
+            dispenseTime.reroll()
+            interTrialTime.reroll()
+            p.xadd(b'state',{b'state':'start_trial',b'time':currTime})
+            state = STATE_START_TRIAL
     
-    p = r.pipeline()
-    p.xadd('cursorLocation', curs.location)
-    p.xadd('targetLocation', tgtDict)
+    
     p.execute()
 
