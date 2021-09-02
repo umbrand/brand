102b7cc00565b35dc7c9a7cf30f93b1a058ae948
yahiaali <yahiaali124@gmail.com>
2021-09-02 14:00:39 -0400

    Load decoderTest yaml parameters when plotting

--- session/decoderTest/analyze.py
+++ session/decoderTest/analyze.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from redis import Redis
+import yaml
 
 try:
     plt.close(plt.figure())
@@ -33,6 +34,25 @@ udf['ts_dec'] = udf['ts_dec'].astype(float)
 udf['ts'] = udf['ts'].astype(float)
 
 # %%
+# Get run info
+YAML_FILE = 'func_generator.yaml'
+
+
+def get_parameter_value(fileName, field):
+    with open(fileName, 'r') as f:
+        yamlData = yaml.safe_load(f)
+
+    for record in yamlData['parameters']:
+        if record['name'] == field:
+            return record['value']
+
+
+sample_rate = get_parameter_value('func_generator.yaml', 'sample_rate')
+n_features = get_parameter_value('func_generator.yaml', 'n_features')
+n_targets = get_parameter_value('func_generator.yaml', 'n_targets')
+decoder_type = get_parameter_value('decoder.yaml', 'decoder_type')
+
+# %%
 fig, ax = plt.subplots(figsize=(6, 4))
 data = np.array([
     udf['ts_dec'] - udf['ts_gen'],  # decoder latency
@@ -43,5 +63,11 @@ ax.violinplot(data.T * 1e3, showmeans=True)
 ax.set_xticks(np.arange(data.shape[0]) + 1)
 ax.set_xticklabels(['Decoder', 'UDP Sender', 'Total'])
 ax.set_ylabel('Latency (ms)')
-plt.savefig('latency.pdf')
+ax.set_title(f'{decoder_type} decoder, {sample_rate} Hz data\n'
+             f'{n_features} neural ch, {n_targets} kinematic ch')
+
+
+plt.savefig(f'latency_{decoder_type.lower()}_{sample_rate}Hz'
+            f'_{n_features}n_{n_targets}k.pdf')
+
 # %%
