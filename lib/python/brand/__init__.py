b3168bc5b5ccf44121ae891c713306a0d371071e
kevin <kb0dkin@github.com>
2021-10-04 19:30:49 +0000

    created new tool to parse the pack/unpack strings from a bytecode string from redis

--- lib/python/brand/__init__.py
+++ lib/python/brand/__init__.py
@@ -1,3 +1,3 @@
 from .tools import (get_node_parameter_value, get_parameter_value,
                          initializeRedisFromYAML, get_node_parameter_dump,
-                         get_redis_info, main, get_node_io)
+                         get_redis_info, main, get_node_io, unpack_string)
