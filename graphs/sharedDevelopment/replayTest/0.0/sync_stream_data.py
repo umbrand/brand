9142e87ef9bad0652f76bcba8d6eeba39a9947b2
Yahia Ali <yahiaali124@gmail.com>
2021-11-04 18:21:30 -0400

    Demonstrate decoding on synced data

--- graphs/sharedDevelopment/replayTest/0.0/sync_stream_data.py
+++ graphs/sharedDevelopment/replayTest/0.0/sync_stream_data.py
@@ -3,8 +3,10 @@ import numpy as np
 import pandas as pd
 import yaml
 from brand import initializeRedisFromYAML
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import train_test_split
 
-from utils import decode_field
+from utils import decode_field, get_lagged_features, smooth_data
 
 # %%
 # Connect to Redis
@@ -15,6 +17,19 @@ with open('stream_spec.yaml', 'r') as f:
     stream_spec = yaml.safe_load(f)
 
 # %%
+# Load data from Redis
+# taskInput
+all_entries = r.xread({b'taskInput': 0})
+stream_entries = all_entries[0][1]
+stream_data = [entry[1] for entry in stream_entries]
+
+task_input = pd.DataFrame(stream_data)
+for field in ['timestamps', 'samples']:
+    task_input[field] = task_input[field.encode()].apply(
+        decode_field, stream='taskInput', field=field, stream_spec=stream_spec)
+task_input = task_input.set_index('timestamps')
+
+# thresholdCrossings
 all_entries = r.xread({b'thresholdCrossings': 0})
 stream_entries = all_entries[0][1]
 stream_data = [entry[1] for entry in stream_entries]
@@ -27,34 +42,21 @@ for field in ['timestamps', 'crossings']:
         field=field,
         stream_spec=stream_spec)
 
-# %%
-all_entries = r.xread({b'taskInput': 0})
-stream_entries = all_entries[0][1]
-stream_data = [entry[1] for entry in stream_entries]
-
-task_input = pd.DataFrame(stream_data)
-for field in ['timestamps', 'samples']:
-    task_input[field] = task_input[field.encode()].apply(
-        decode_field,
-        stream='taskInput',
-        field=field,
-        stream_spec=stream_spec)
-task_input = task_input.set_index('timestamps')
-
-# %%
+# Separate channels into their own columns
 tc_timestamps = threshold_crossings['timestamps'].values
 crossings = np.stack(threshold_crossings['crossings'])
 n_chans = crossings.shape[1]
 
 channel_labels = [f'ch{i :03d}' for i in range(n_chans)]
-df = pd.DataFrame(crossings,
-                  index=tc_timestamps + 13,
-                  columns=channel_labels)
+tc_df = pd.DataFrame(crossings,
+                     index=tc_timestamps + 13,
+                     columns=channel_labels)
 
 # %%
-joined_df = task_input.join(df, how='inner')
+joined_df = task_input.join(tc_df, how='inner')
 joined_df.index = pd.to_timedelta(joined_df.index / 30, unit='ms')
 joined_df
+
 # %%
 samples = np.stack(joined_df['samples'])
 joined_df['touch'] = samples[:, 0]
@@ -62,6 +64,24 @@ joined_df['x'] = samples[:, 1]
 joined_df['y'] = samples[:, 2]
 
 # %%
-binned_data = joined_df.resample('5ms').sum()
+bin_size_ms = 5  # ms
+gauss_width_ms = 20  # ms, for smoothing
+binned_data = joined_df.resample(f'{bin_size_ms :d}ms').sum()
+
+# %%
+smoothed_data = smooth_data(binned_data[channel_labels].values,
+                            bin_size=bin_size_ms,
+                            gauss_width=gauss_width_ms)
+
+X = get_lagged_features(smoothed_data, n_history=3)
+y = binned_data[['x', 'y']].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+mdl = Ridge()
+mdl.fit(X_train, y_train)
+
+print(f'Train R^2: {mdl.score(X_train, y_train)}')
+print(f'Test R^2: {mdl.score(X_test, y_test)}')
 
 # %%
--- graphs/sharedDevelopment/replayTest/0.0/utils.py
+++ graphs/sharedDevelopment/replayTest/0.0/utils.py
@@ -1,6 +1,8 @@
 from ctypes import Structure, c_long
 from datetime import datetime
+
 import numpy as np
+from scipy import signal
 
 
 class timeval(Structure):
@@ -52,3 +54,56 @@ def decode_field(entry, stream, field, stream_spec):
         if len(decoded_entry) == 1:
             decoded_entry = decoded_entry.item()
     return decoded_entry
+
+
+def get_lagged_features(data, n_history: int = 3):
+    """
+    Lag the data along the time axis. Stack the lagged versions of the data
+    along the feature axis.
+    Parameters
+    ----------
+    data : array of shape (n_samples, n_features)
+        Data to be lagged
+    n_history : int, optional
+        Number of bins of history to lag the data by, by default 3
+    Returns
+    -------
+    lagged_features : array of shape (n_samples, (n_history + 1) * n_features)
+        Lagged version of the original data
+    """
+    lags = [None] * (n_history + 1)
+    for i in range(n_history + 1):
+        lags[i] = np.zeros_like(data)
+        lags[i][i:, :] = data[:-i, :] if i > 0 else data
+    lagged_features = np.hstack(lags)
+    return lagged_features
+
+
+def smooth_data(data, bin_size, gauss_width):
+    """
+    Smooth data with a Gaussian kernel
+
+    Parameters
+    ----------
+    data : np.ndarray
+        Array of data with shape (n_bins, n_features)
+    bin_size : int
+        Bin size of data (in ms)
+    gauss_width : int
+        Standard deviation of the Gaussian kernel (in ms)
+    """
+    smoothed_data = np.empty_like(data)
+
+    gauss_bin_std = gauss_width / bin_size
+    win_len = int(6 * gauss_bin_std)
+
+    window = signal.gaussian(win_len, gauss_bin_std, sym=True)
+    window /= np.sum(window)
+
+    n_chans = data.shape[1]
+    for ich in range(n_chans):
+        smoothed_data[:, ich] = signal.convolve(data[:, ich],
+                                                window,
+                                                mode='same')
+
+    return smoothed_data
