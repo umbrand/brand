4d827b65aa16f64bc897b3f26f944268579158ca
kevin <kb0dkin@github.com>
2021-11-12 11:26:04 -0600

    trying to run plotter with the new BRANDS struture

--- nodes/plotter/plotter_x_display.sh
+++ nodes/plotter/plotter_x_display.sh
@@ -3,7 +3,7 @@
 if [ -z $DISPLAY ] # if there's no display
     then # then run xinit
         echo "trying to connect with X"
-        xinit ../../bin/plotter $* -- vt$XDG_VTNR
+        xinit ./plotter.bin $* -- vt$XDG_VTNR
     else # else skip it
         python3 ./plotter.py $*
 fi
