652f1777f7811aea85c129e48b60d70f563bcfe4
kevin <kb0dkin@github.com>
2022-02-24 15:56:22 -0600

    changes to get wiener filter and filter builder working properly

--- nodes/decoder_trainer/decoder_trainer.py
+++ nodes/decoder_trainer/decoder_trainer.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 # decoder_trainer.py
 # %%
-import json
+import json # likely going to switch to pickle
+import pickle
 import logging
 import sys
 from ctypes import Structure, c_long
@@ -12,6 +13,19 @@ import numpy as np
 import pandas as pd
 from brand import get_node_parameter_value, initializeRedisFromYAML
 from sklearn.linear_model import Ridge
+
+
+# Current status:
+# For some reason it is bringing the 96xT data in as a 96*T one dimensional vector.
+# This needs to change if we're going to actually be able to do predictions.
+#
+# 1. set up reshape properly
+# 2. allow for lags to have history in the ridge regression
+# 
+# -- Kevin
+
+
+
 
 
 class timeval(Structure):
@@ -64,7 +78,7 @@ def bin_data(data, binsize=5):
 
 def save_model(estimator, filepath):
     """
-    Save a JSON representation of a scikit-learn model to the provided filepath
+    Save a pkl representation of a scikit-learn model to the provided filepath
 
     Parameters
     ----------
@@ -82,7 +96,7 @@ def save_model(estimator, filepath):
                 val = val.tolist()
             model_info['attr'][key] = val
     with open(filepath, 'w') as f:
-        json.dump(model_info, f, indent=4)
+        pickle.dump(model_info, f, indent=4)
 
 
 def load_model(estimator, filepath):
@@ -114,7 +128,6 @@ def load_model(estimator, filepath):
 
 
 YAML_FILE = sys.argv[1] if len(sys.argv) > 1 else 'decoder_trainer.yaml'
-YAML_FILE = 'decoder_trainer.yaml'  # jupyter
 NAME = 'decoder_trainer'
 
 # set up logging
@@ -133,14 +146,18 @@ r = initializeRedisFromYAML(YAML_FILE, NAME)
 # %%
 # Load Neural Data
 # xread STREAMS thresholdCrossings 0
-reply = r.xread(streams={'thresholdCrossings': 0})
+
+i_stream = get_node_parameter_value(YAML_FILE, NAME, 'trainStream')
+reply = r.xread(streams={i_stream: 0})
 
 stream_name, entries = reply[0]
 entry_list = []
+trainSamp = get_node_parameter_value(YAML_FILE, NAME, 'trainSamp')
+trainTS = get_node_parameter_value(YAML_FILE, NAME, 'trainTS')
 for entry_id, entry_dict in entries:
-    entry_dict[b'crossings'] = np.frombuffer(entry_dict[b'crossings'],
+    entry_dict[trainSamp] = np.frombuffer(entry_dict[trainSamp],
                                              dtype=np.short)
-    entry_dict[b'timestamps'] = np.frombuffer(entry_dict[b'timestamps'],
+    entry_dict[trainTS] = np.frombuffer(entry_dict[trainTS],
                                               dtype=np.uint32).item()
     entry_dict[b'id'] = entry_id
     entry_list.append(entry_dict)
@@ -150,16 +167,13 @@ spike_df.rename(columns={col: col.decode()
                          for col in spike_df.columns},
                 inplace=True)
 
-spikes = np.stack(spike_df['crossings'].values)
-# bin spikes
-sample_rate = 1e3  # Hz
-binsize = 5
-binned_spikes = bin_data(spikes, binsize)
 
 # %%
 # Load cursor kinematics
 # XREAD COUNT 1 STREAMS taskInput 0
-reply = r.xread(streams={b'taskInput': 0})
+targetStream = get_node_parameter_value(YAML_FILE, NAME, 'targetStream')
+targetSamples = get_node_parameter_value(YAML_FILE, NAME, 'targetSamp')
+reply = r.xread(streams={targetStream: 0})
 stream_name, entries = reply[0]
 entry_list = []
 
@@ -170,7 +184,7 @@ for entry_id, entry_dict in entries:
         entry_dict[b'BRANDS_time']).timestamp()
     entry_dict[b'udp_recv_time'] = timeval_to_datetime(
         entry_dict[b'udp_recv_time']).timestamp()
-    entry_dict[b'samples'] = np.frombuffer(entry_dict[b'samples'],
+    entry_dict[targetSamples] = np.frombuffer(entry_dict[targetSamples],
                                            dtype=np.short)
     entry_list.append(entry_dict)
 
@@ -179,15 +193,37 @@ kin_df.rename(columns={col: col.decode()
                        for col in kin_df.columns},
               inplace=True)
 
-pos = np.stack(kin_df['samples'])
-kin_df['x_pos'] = pos[:, 0]
-kin_df['y_pos'] = pos[:, 1]
+pos = np.stack(kin_df[targetSamples])
+kin_df['x_pos'] = pos[:, 1] # touchpad is sensor 0, so x is sensor 1 and y is sensor 2
+kin_df['y_pos'] = pos[:, 2]
 
-# %%
-# Truncate cursor kinematics to match the size of neural data
-# TODO: Sync the neural data with the kinematic data
-# TODO: Split the data into trials
-kin_df = kin_df[:spike_df.shape[0]]
+# ----------------------------------------------------------
+# align spikes and target signal 
+
+# first align the beginning
+if kin_df.iloc[0][targetTS] < spike_df.iloc[0][trainTS]:
+    lag = np.where(kin_df[targetTS] == spike_df.iloc[0][trainTS]) # lag for crossings to start
+    kin_df = kin_df.iloc[lag]
+else
+    lag = np.where(spike_df[trainTS] == kin_df.iloc[0][targetTS]) # lag for crossings to start
+    spike_df = spike_df.iloc[lag]
+
+# trim to match lengths
+if len(kin_df) > len(spike_df):
+    kin_df = kin_df.iloc[:len(spike_df)]
+else:
+    spike_df = spike_df.iloc[:len(kin_df)]
+    
+
+# bin train and target datasets
+sample_rate = get_node_parameter_value(YAML_FILE, NAME, 'sample_rate')  # Hz
+binsize = get_node_parameter_value(YAML_FILE, NAME, 'binsize')
+
+spikes = np.stack(spike_df[trainSamp].values)
+binned_spikes = bin_data(spikes, binsize)
+
+binned_kin = bin_data(
+
 
 # %%
 # define the decoder inputs
