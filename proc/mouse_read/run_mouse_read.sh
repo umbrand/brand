c4a0c91802c9d0839385cfc24bb82d6afc285e1f
Mattia Rigotti <mirigotti@gmail.com>
2022-07-07 14:13:17 -0400

    Delete proc, session, test, and run folders

--- proc/mouse_read/run_mouse_read.sh
+++
@@ -1,6 +0,0 @@
-# ID=$(xinput list --id-only "USB Optical Mouse")
-# ## disable the mouse as a user input
-# echo "sudo xinput set-prop $ID \"Device Enabled\" 0"
-# sudo xinput set-prop $ID "Device Enabled" 0
-
-sudo nice -n -20 ./usbMouse.out /dev/input/by-id/usb-Razer_Razer_Viper-event-mouse 1
