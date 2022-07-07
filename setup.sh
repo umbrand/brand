5f03f4dacfc03df5e05e88852e9ebdecd611c31e
Yahia Ali <yahiaali124@gmail.com>
2022-07-07 12:05:06 -0400

    Make unix socket folder in setup.sh

--- setup.sh
+++ setup.sh
@@ -23,6 +23,12 @@ export RDB_SAVE_DIR # push up to the environment
 BRAND_BASE_DIR=$(pwd)
 export BRAND_BASE_DIR # save brand base dir to the environment
 
+# create directory for redis socket
+if [ ! -d "/var/run/redis" ]; then # if the directory doesn't exist, create it
+    sudo mkdir "/var/run/redis"
+fi
+sudo chmod -R 777 "/var/run/redis"
+
 ################################################
 # defining functions
 ################################################
