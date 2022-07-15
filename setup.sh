11cc2f79d88aca3d653667d8a2c295539ee4e9d5
Yahia Ali <yahiaali124@gmail.com>
2022-07-15 18:31:00 -0400

    Upgrade to Redis v7.0.3

--- setup.sh
+++ setup.sh
@@ -90,3 +90,5 @@ conda activate rt
 # Make aliases for booter and supervisor
 alias booter='sudo -E env "PATH=$PATH" python -m brand.booter'
 alias supervisor='sudo -E env "PATH=$PATH" python supervisor/supervisor.py'
+
+export PATH=$(pwd)/bin:$PATH
