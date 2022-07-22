699e6b78c9e9e63bf4c9ee1493ddf4a9f3ea7b71
Yahia Ali <yahiaali124@gmail.com>
2022-07-22 18:04:18 -0400

    Run func_generator for finite duration Encode timestamps and indices as bytes

--- nodes/func_generator/func_generator.py
+++ nodes/func_generator/func_generator.py
@@ -10,14 +10,25 @@ import json
 
 from brand import BRANDNode
 
+
 class FunctionGenerator(BRANDNode):
+
     def __init__(self):
-        
+
         super().__init__()
-        
+
         self.sample_rate = self.parameters['sample_rate']
         self.n_features = self.parameters['n_features']
         self.n_targets = self.parameters['n_targets']
+        self.stop_graph_when_done = (self.parameters['stop_graph_when_done']
+                                     if 'stop_graph_when_done'
+                                     in self.parameters else False)
+        if ('duration' in self.parameters
+                and self.parameters['duration'] is not None):
+            self.duration = self.parameters['duration']
+        else:
+            self.duration = np.inf
+        self.total_samples = np.floor(self.duration * self.sample_rate)
 
         # initialize output stream entry data
         self.i = 0
@@ -27,10 +38,10 @@ class FunctionGenerator(BRANDNode):
         self.syncDictJson = json.dumps(self.syncDict)
 
         self.stream_entry = {
-            'ts': time.monotonic(), 
+            'ts': time.monotonic(),
             'sync': self.syncDictJson.encode(),
             'samples': self.x.tobytes(),
-            'i': self.i    
+            'i': self.i
         }
 
         logging.info('Starting function generator...')
@@ -45,16 +56,16 @@ class FunctionGenerator(BRANDNode):
 
     def send_sample(self):
         self.x = np.sin(self.t_arr + (self.i * 0.05 * 2 * np.pi),
-                   dtype=np.float64).dot(self.A.T)
+                        dtype=np.float64).dot(self.A.T)
 
         self.syncDict = {'i': self.i}
         self.syncDictJson = json.dumps(self.syncDict)
 
         self.stream_entry = {
-            'ts': time.monotonic(), 
+            'ts': np.uint64(time.monotonic_ns()).tobytes(),
             'sync': self.syncDictJson.encode(),
             'samples': self.x.tobytes(),
-            'i': self.i    
+            'i': np.uint64(self.i).tobytes()
         }
 
         self.r.xadd('func_generator', self.stream_entry)
@@ -64,16 +75,20 @@ class FunctionGenerator(BRANDNode):
     def run(self):
 
         self.build()
-        
+
         logging.info('Sending data...')
 
         last_time = 0
         # send samples to Redis at the specified sampling rate
-        while True:
+        while self.i < self.total_samples:
             current_time = time.perf_counter()
             if current_time - last_time >= 1 / self.sample_rate:
                 self.send_sample()
                 last_time = current_time
+
+        if self.stop_graph_when_done:
+            time.sleep(1)
+            self.r.xadd('supervisor_ipstream', {'commands': 'stopGraph'})
 
 
 if __name__ == "__main__":
