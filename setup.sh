ecfb1f635984544b02d21b13e935dc07261bcc98
Yahia Ali <yahiaali124@gmail.com>
2022-07-22 16:41:50 -0400

    Remove mkdir for unix socket directory

--- setup.sh
+++ setup.sh
@@ -7,18 +7,6 @@
 BRAND_BASE_DIR=$(pwd)
 export BRAND_BASE_DIR # save brand base dir to the environment
 
-# create directory for redis socket
-if [ ! -d "/var/run/redis" ]; then # if the directory doesn't exist, create it
-    sudo mkdir "/var/run/redis"
-fi
-sudo chmod -R 777 "/var/run/redis"
-
-################################################
-# defining functions
-################################################
-
-
-
 # Activate the rt environment to get to work
 conda activate rt
 
