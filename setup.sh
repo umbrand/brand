4f732c81ca5d71ea202a982933fcbe2a5b4002f6
Luis Cubillos <lhcubill@umich.edu>
2025-07-20 13:16:46 -0400

    added supervisor edits for saving files and preventing closing

--- setup.sh
+++ setup.sh
@@ -6,6 +6,19 @@
 
 BRAND_BASE_DIR=$(pwd)
 BRAND_MOD_DIR=$BRAND_BASE_DIR/../brand-modules/
+# Check if server path exists, if not mount the disk
+export SERVER_PATH=/mnt/z/Data
+if [ ! -d "$SERVER_PATH" ]; then
+    # Check if we're in WSL or native Linux
+    if grep -qi microsoft /proc/version 2>/dev/null; then
+        # WSL environment - use drvfs to mount Windows drive
+        sudo mkdir -p /mnt/z && sudo mount -t drvfs Z: /mnt/z
+    else
+        # Native Linux environment - use different mount command or approach
+        read -p "Enter username for CNPL server: " username
+        sudo mkdir -p /mnt/z && sudo mount -t cifs //cnpl-drmanhattan.engin.umich.edu/share /mnt/z -o user=$username,domain=UMROOT,vers=3.0
+    fi
+fi
 export BRAND_BASE_DIR # save brand base dir to the environment
 
 # Activate the rt environment to get to work
@@ -14,5 +27,8 @@ conda activate rt
 # Make aliases for booter and supervisor
 alias booter='sudo -E env "PATH=$PATH" python supervisor/booter.py'
 alias supervisor='sudo -E env "PATH=$PATH" python supervisor/supervisor.py'
+
+# useful alias for WSL to mount server
+alias mount_server='sudo mkdir /mnt/z && sudo mount -t drvfs Z: /mnt/z'
 
 export PATH=$(pwd)/bin:$PATH
